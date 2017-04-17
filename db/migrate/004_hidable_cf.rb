08325e9eb6d78317192882d0077e12cb1e5818e2
Florian Eck <florian.eck@el-digital.de>
2017-04-17 13:35:53 +0200

    removed old migrations

--- db/migrate/004_hidable_cf.rb
+++ db/migrate/004_hidable_cf.rb
@@ -1,16 +1,6 @@
 class HidableCf < ActiveRecord::Migration
   def up
 
-    unless table_exists?(:akquinet_time_entry_custom_fields_activities)
-
-      create_table :akquinet_time_entry_custom_fields_activities, :id => false do |t|
-
-        t.column 'custom_field_id', :integer, {:null => false}
-        t.column 'time_entry_activity_id', :integer, {:null => false}
-
-      end
-    end
-
     unless column_exists? :custom_fields, 'akquinet_extra_money_multiplied'
       add_column :custom_fields, 'akquinet_extra_money_multiplied', :boolean, {:null => false, :default => false}
     end
@@ -22,8 +12,6 @@ class HidableCf < ActiveRecord::Migration
   end
 
   def down
-
-    drop_table :akquinet_time_entry_custom_fields_activities
 
     remove_column :custom_fields, 'akquinet_extra_money_multiplied'
     remove_column :custom_fields, 'akquinet_extra_money_offset'
