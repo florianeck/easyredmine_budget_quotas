387b71c41d10ba493a674b22cd024032b6f08749
Florian Eck <florian.eck@el-digital.de>
2018-04-04 16:30:56 +0200

    cleanup query for time entries

--- after_init.rb
+++ after_init.rb
@@ -1,9 +1,7 @@
 Dir[File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/*/*/*.rb'].each {|file| require_dependency file }
 
 require_dependency File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/easy_lookup_budget_quota_activity'
-require_dependency File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/easy_lookup_time_entry'
 require_dependency File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/easy_activity_query'
-require_dependency File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/easy_budget_quota_time_entry_query'
 require_dependency File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/modal_selector_controller_extension'
 
 ActionDispatch::Reloader.to_prepare do
@@ -12,7 +10,6 @@ end
 
 EasyExtensions::EasyLookups::EasyLookup.map do |easy_lookup|
   easy_lookup.register EasyPatch::EasyLookupBudgetQuotaActivity.new
-  easy_lookup.register EasyPatch::EasyLookupTimeEntry.new
 end
 
 ModalSelectorsController.send :include, EasyPatch::ModelSelectorControllerExtension
--- lib/easyredmine_budget_quotas/easy_patch/modal_selector_controller_extension.rb
+++ lib/easyredmine_budget_quotas/easy_patch/modal_selector_controller_extension.rb
@@ -20,28 +20,5 @@ module EasyPatch
       end
     end
     
-    def budget_quota_time_entry
-      retrieve_query(EasyBudgetQuotaTimeEntryQuery)
-      @query.name = l("label_time_entry")
-
-      set_query(@query)
-      
-      
-      @query.add_filter("activity_id", "=", TimeEntryActivity.where(internal_name: ["ebq_quota", "ebq_budget"]).pluck(:id).map(&:to_s))
-      @query.add_filter("project_id", "=", params[:modal_project_id])
-      
-      prepare_easy_query_render
-
-      if request.xhr? && !@entities
-        render_404
-        return false
-      end
-
-      if loading_group?
-        render_easy_query_html(@query, nil, {:selected_values => prepare_selected_values})
-      else
-        render_modal_selector_entities_list(@query, @entities, @entity_pages, @entity_count)
-      end
-    end
   end
 end
