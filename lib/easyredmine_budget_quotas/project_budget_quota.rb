ec946ee796ed68b9cfef3930158403df32ac11e5
Florian Eck <florian.eck@el-digital.de>
2019-07-01 06:58:57 +0200

    allow to check used budgets among multiple projects

--- lib/easyredmine_budget_quotas/project_budget_quota.rb
+++ lib/easyredmine_budget_quotas/project_budget_quota.rb
@@ -23,7 +23,7 @@ module EasyredmineBudgetQuotas
       query << "INNER JOIN `custom_values` ON `custom_values`.`customized_id` = `time_entries`.`id` AND `custom_values`.`customized_type` = 'TimeEntry'"
       query << "INNER JOIN `custom_fields` on `custom_fields`.`id` = `custom_values`.`custom_field_id`"
       query << "INNER JOIN `easy_money_time_entries_expenses` ON `easy_money_time_entries_expenses`.`time_entry_id` = `time_entries`.`id`"
-      query << "WHERE `custom_fields`.`internal_name` = 'ebq_budget_quota_id' AND `custom_values`.`value` = '#{current_entry.id}' AND `time_entries`.`project_id` = #{self.id}"
+      query << "WHERE `custom_fields`.`internal_name` = 'ebq_budget_quota_id' AND `custom_values`.`value` = '#{current_entry.id}'"
       query << "AND time_entries.activity_id NOT IN (#{(EasyredmineBudgetQuotas.budget_entry_activities.ids + EasyredmineBudgetQuotas.quota_entry_activities.ids).join(",")})"
       query << "AND `easy_money_time_entries_expenses`.`rate_type_id` = #{self.calculation_rate_id}"
 
@@ -34,7 +34,7 @@ module EasyredmineBudgetQuotas
       EasyMoneyRateType.find_by(name: self.budget_quotas_money_rate_type).id
     end
 
-    
+
 
   end
 end
